ef53d172ecfc91149ebc89122fbaddf147eb91a4
darnolf <arnolf10@gmail.com>
2017-12-26 01:31:29 +0200

    done dev

--- src/App.js
+++ src/App.js
@@ -2,20 +2,36 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+class RefComponent extends Component {
+  constructor(props) {
+    super(props);
+    this.display = this.display.bind(this)
+  }
+ 
+ display() {
+   const name =  this.inputdemo.value
+   document.getElementById('dispName').innerHTML = name
+ }
+
+  render() { 
+    return ( <div>
+      <h2>Name: <input type="text" ref = {input =>this.inputdemo = input}/></h2>
+      <button onClick = {this.display}>Click</button>
+      <h1>Hi ... <span id="dispName"></span></h1>
+    </div> )
+  }
+}
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <RefComponent/>
       </div>
     );
   }
 }
 
 export default App;
+
+
